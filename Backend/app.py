a2f324ab1f94f9e9d94059e90cf813a706eab7c0
Andres Dorado <andres.dorado90@gmail.com>
2021-06-05 19:37:09 +0200

    test and steps endpoints done, user session in progress

--- Backend/app.py
+++ Backend/app.py
@@ -71,18 +71,17 @@ def handle_500_error(_error):
 def create():
     try:
         if request.method == 'POST':
-            if all(k in request.json for k in ("test_id", "name", "description")):
-
-                new_test = {'_id': request.json["test_id"],  # test_id correspond to the _id in mongodb
+            if all(k in request.json for k in ("name", "description")):
+                new_test = {'_id': uuid.uuid4(),  # test_id correspond to the _id in mongodb
                             'name': request.json["name"],
                             'description': request.json["description"],
                             'consent': request.json["consent"] if keyExist("consent", request.json) else "",
-                            'number_of_steps': request.json["number_of_steps"] if keyExist("number_of_steps", request.json) else 0,
-                            'steps': request.json["steps"] if keyExist("steps", request.json) else [],
+                            'number_of_steps': 0,
+                            'steps': [],
                             'published': False,
                             'creation_date': datetime.datetime.now()}
                 Test.insert_one(new_test)
-                return make_response(jsonify(message="Test created successfully"), 200)
+                return make_response(jsonify(message="Test created successfully", test_id=new_test['_id']), 200)
             else:
                 abort(400)
     except BulkWriteError as e:
@@ -96,8 +95,11 @@ def create():
 @app.route('/test/<test_id>', methods=['GET', 'DELETE'])
 def test_operations(test_id):
     try:
+        # convert test_id to test_uuid
+        test_uuid = uuid.UUID(test_id)
+
         if request.method == 'GET':
-            test = Test.find_one({"_id": test_id})
+            test = Test.find_one({"_id": test_uuid})
             if test is None:
                 return make_response(jsonify(message="Test not found"), 404)
             else:
@@ -105,29 +107,32 @@ def test_operations(test_id):
                 return make_response(jsonify(test), 200)
 
         if request.method == 'DELETE':
-            result = Test.find_one_and_delete({"_id": test_id})
+            result = Test.find_one_and_delete({"_id": test_uuid})
             if result is None:
                 return make_response(jsonify(message="Test not found"), 404)
             else:
                 # Delete all steps associated to the test
-                Step.delete_many({"test_id": test_id})
-                return make_response(jsonify(message="Test deleted successfully"), 200)
+                Step.delete_many({"test_id": test_uuid})
+                return make_response(jsonify(message="Test deleted successfully", test_id=test_uuid), 200)
     except BulkWriteError as e:
         return make_response(jsonify(message="Error",
                                      details=e.details), 500)
 
 
-# CRUD oparation for Steps
+# CRUD operation for Steps
 ##########################
-@app.route('/test/<test_id>/steps', methods=['GET', 'POST'])
+@app.route('/test/<test_id>/steps', methods=['GET'])
 def get_all_steps(test_id):
     try:
         if test_id is None:
             abort(400)
             return
 
+        # convert test_id to test_uuid
+        test_uuid = uuid.UUID(test_id)
+
         if request.method == 'GET':
-            steps = Step.find({'test_id': test_id})
+            steps = Step.find({'test_id': test_uuid})
             if steps is None:
                 return make_response(jsonify(message="Steps not found"), 404)
             else:
@@ -137,25 +142,26 @@ def get_all_steps(test_id):
                                      details=e.details), 500)
 
 
-@app.route('/test/<test_id>/step/<step_id>', methods=['PUT', 'DELETE', 'GET'])
+@app.route('/test/<test_id>/step/<step_id>', methods=['PUT', 'DELETE', 'GET', 'POST'])
 def step_operations(test_id, step_id):
     try:
         if test_id is None:
             abort(400)
             return
-        # convert step_id to step_uuid
-        step_uuid = uuid.UUID(step_id)
+
+        # convert test_id to test_uuid
+        test_uuid = uuid.UUID(test_id)
 
         if request.method == 'POST':
             new_step = {'_id': uuid.uuid4(),
-                        'test_id': test_id,  # test_id correspond to the _id in mongodb
+                        'test_id': test_uuid,  # test_id correspond to the _id in mongodb
                         'name': request.json["name"],
                         'type': request.json["type"],
                         'duration': request.json["duration"] if keyExist("duration", request.json) else 30,
                         'content': {}}
             # create step in current test
             test = Test.update_one(
-                {'_id': test_id}, {'$push': {'steps': new_step['_id']}})
+                {'_id': test_uuid}, {'$push': {'steps': new_step['_id']}})
             # create new step
             Step.insert_one(new_step)
             if test.matched_count == 0:
@@ -163,10 +169,13 @@ def step_operations(test_id, step_id):
             elif test.modified_count == 0:
                 return make_response(jsonify(message="Step not added"), 500)
             else:
-                return make_response(jsonify(message=f"Step {new_step['_id']} added successfully to test {test_id}"), 200)
+                return make_response(jsonify(message=f"Step added successfully to the test", test_id=test_uuid, step_id=new_step['_id']), 200)
+
+        # convert step_id to step_uuid
+        step_uuid = uuid.UUID(step_id)
 
         if request.method == 'GET':
-            step = Step.find_one({'_id': step_uuid, 'test_id': test_id})
+            step = Step.find_one({'_id': step_uuid, 'test_id': test_uuid})
             if step is None:
                 return make_response(jsonify(message="Steps not found"), 404)
             else:
@@ -178,17 +187,28 @@ def step_operations(test_id, step_id):
             __parameters = ['name', 'type', 'duration', 'content']
             # build the data to send to db
             for param in __parameters:
-                if (keyExist(param, request.json)):
+                if keyExist(param, request.json):
                     updated_step[param] = request.json[param]
             # update step in db
-            step = Step.update_one({'_id': step_uuid, 'test_id': test_id}, {
+            step = Step.update_one({'_id': step_uuid, 'test_id': test_uuid}, {
                 '$set': updated_step})
             if step.matched_count == 0:
                 return make_response(jsonify(message="Step not found"), 404)
             elif step.modified_count == 0:
                 return make_response(jsonify(message="Step not updated"), 500)
             else:
-                return make_response(jsonify(message=f"Step {step_id} updated successfully to test {test_id}"), 200)
+                return make_response(jsonify(message=f"Step updated successfully", test_id=test_uuid, step_id=step_uuid), 200)
+
+        if request.method == 'DELETE':
+            result = Step.find_one_and_delete(
+                {'_id': step_uuid, 'test_id': test_uuid})
+            if result is None:
+                return make_response(jsonify(message="Resource not found"), 404)
+            else:
+                # Remove step identifier from Test[test_id].steps
+                Test.update_one(
+                    {'_id': test_uuid}, {'$pull': {'steps': step_uuid}})
+                return make_response(jsonify(message="Test step deleted successfully", test_id=test_uuid, step_id=step_uuid), 200)
     except BulkWriteError as e:
         return make_response(jsonify(message="Error creating test",
                                      details=e.details), 500)
@@ -205,7 +225,7 @@ if __name__ == '__main__':
 
     if ARGS.debug:
         print("Running in debug mode")
-        # cross origing support
+        # cross origen support
         CORS(app)
         app.run(host='0.0.0.0', port=PORT, debug=True)
     else:
