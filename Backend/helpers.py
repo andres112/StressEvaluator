e1c9003056d6def6c5499eaf192b05affe47337b
Andres Dorado <andres.dorado90@gmail.com>
2021-11-16 00:38:47 +0100

    helpers updated in Backend to inlcude session id in partial results reports

--- Backend/helpers.py
+++ Backend/helpers.py
@@ -29,24 +29,34 @@ def groupBy(array):
     # Get the responses only and remove results with empty responses
     responses = np.array([item['responses'] for item in array if (item['responses'] and item['close_date'])],
                          dtype=object).flatten()
+    sessions = np.array([item['_id'] for item in array if (item['responses'] and item['close_date'])],
+                        dtype=object)
     sorted_responses = sorted(responses, key=lambda res: res['step_id'])
     groups = groupby(sorted_responses, lambda content: content['step_id'])
     step_groups = {}
     for step_id, group in groups:
         step_content = []
+        i = 0
         for content in group:
+            content['session_id'] = sessions[i]
             step_content = np.append(step_content, content)
+            i = i+1
         step_groups[str(step_id)] = step_content.tolist()
     return step_groups
 
 
 def jsonToCsv(responses, path):
     content_list = [item['content'] for item in responses]
+    sessions = [item['session_id'] for item in responses]
     # validate if content is list type
     if isinstance(responses[0]['content'], list):
         flat_list = [val for sublist in content_list for val in sublist]
     else:
         flat_list = content_list
+    i = 0
+    for x in range(0, len(flat_list), int(len(flat_list)/len(sessions))):
+        flat_list.insert(x+i, {'session_id':sessions[i]})
+        i = i+1
     df = pd.DataFrame.from_dict(flat_list)
     step_id = responses[0]['step_id']
     # Create temporal directory for csv files
